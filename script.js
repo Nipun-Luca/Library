78803c326a3845fe144ec0bbc0c0f0ae4613b4fd
Nipun-Luca <lmuhudugama@gmail.com>
2023-10-04 22:36:34 +0100

    CSS changes for table and delete button

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const showForm = document.getElementById("showForm");
 const closeButton = document.getElementById("closeButton");
 const booksContainer = document.querySelector(".books-container");
 
+
+//Button to show modal containing the form
 addBookButton.addEventListener("click", () => {
     showForm.showModal();
 });
@@ -22,6 +24,9 @@ closeButton.addEventListener("click", () => {
     showForm.close();
 });
 
+
+//Clicking outside of dialog also closes it
+//Reference: https://blog.webdevsimplified.com/2023-04/html-dialog/
 showForm.addEventListener("click", (e) => {
     const dialogDimensions = showForm.getBoundingClientRect();
     if (
@@ -33,53 +38,55 @@ showForm.addEventListener("click", (e) => {
         showForm.close();
     }
 });
+//Reference
+
 
 const form = document.querySelector("#showForm form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // Get form values
+    //Get form values
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = parseInt(document.getElementById("pages").value);
     const read = document.getElementById("read").checked;
 
-    // Create a new book and add it to the library
+    //Create a new book and add it to the library
     const book = new Book(title, author, pages, read);
     Library.push(book);
 
-    // Hide the form
+    //Hide the form
     showForm.close();
 
-    // Clear the form inputs
+    //Clear the form inputs
     form.reset();
 
-    // Display the books in the table
+    //Display the books in the table
     displayBooks();
 });
 
 function displayBooks() {
-    // Clear the existing rows
+    //Clear the existing rows
     while (booksContainer.firstChild) {
         booksContainer.removeChild(booksContainer.firstChild);
     }
 
-    // Loop through the library and display each book as a table row
+    //Loop through the library and display each book as a table row
     Library.forEach((book, index) => {
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.pages}</td>
-            <td><button class="read-button" data-index="${index}">${book.read ? 'Read' : 'Unread'}</button></td>
-            <td><button onclick="deleteBook(${index})">X</button></td>
+            <td><button class="read-button" data-index="${index}">${book.read ? 'READ' : 'UNREAD'}</button></td>
+            <td><button class="delete" onclick="deleteBook(${index})">DELETE</button></td>
         `;
 
-        // Add event listener to the read button
+        //Add event listener to the read button
         const readButton = row.querySelector('.read-button');
         readButton.addEventListener('click', () => toggleReadStatus(index));
 
-        // Style the read button
+        //Style the read button
         if (book.read) {
             readButton.classList.add('read');
         } else {
@@ -91,23 +98,23 @@ function displayBooks() {
 }
 
 function toggleReadStatus(index) {
-    // Toggle the read status for the book at the given index
+    //Toggle the read status for the book at the given index
     Library[index].read = !Library[index].read;
 
-    // Update the displayed books
+    //Update the displayed books
     displayBooks();
 }
 
 function deleteBook(index) {
-    // Remove the book from the library
+    //Remove the book from the library
     Library.splice(index, 1);
 
-    // Update the displayed books
+    //Update the displayed books
     displayBooks();
 }
 
-// Initial display of books
+//Initial display of books
 displayBooks();
 
-// Handle window resize
+//Handle window resize
 window.addEventListener('resize', displayBooks);
